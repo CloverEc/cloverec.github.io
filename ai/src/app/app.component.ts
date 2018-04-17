0c407259a25a8165ecf48306458efa2142e54ec9
watase <clover.ec.2010@gmail.com>
2018-04-17 16:42:43 +0800

    ai

--- ai/src/app/app.component.ts
+++ ai/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, DoCheck, ViewChild,ElementRef} from '@angular/core';
+import {AfterViewInit, Component, ViewChild,ElementRef} from '@angular/core';
 import * as ndarray from 'ndarray'
 import * as KerasJS from 'keras-js'
 import * as ops  from 'ndarray-ops'
@@ -7,37 +7,31 @@ import * as ops  from 'ndarray-ops'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  implements AfterViewInit, DoCheck {
+export class AppComponent  implements AfterViewInit{
   title = 'app'
   reslt = ""
-   //context: CanvasRenderingContext2D;
-  // @ViewChild('myCanvas') myCanvas;
   @ViewChild('myCanvas') canvasElm: ElementRef;
   @ViewChild('video') videoElm: ElementRef;
-  private captureData: string;
-
-
+  private captureData: any;
+  private  model:any 
   readonly medias: MediaStreamConstraints = {audio: false, video: {
-      width: { min:  480, ideal: 500, max: 1000 },
-      height: { min: 480, ideal: 500, max: 1000 },
+      width: { min:  100, ideal: 100, max: 100 },
+      height: { min: 100, ideal: 100, max: 100 },
   }};
 
     ngAfterViewInit() {
-            // const canvas = this.myCanvas.nativeElement;
-            // this.context = canvas.getContext('2d');
-            this.startCamera();
-
-            // setTimeout(()=>{
-              //  this.draw()
-            // },100)
-        }
-    //
-    ngDoCheck() {
-        // this.draw();
+     this.startCamera();
+     this.model = new KerasJS.Model({
+         filepath: '/assets/models/model1d2.bin',
+         transferLayerOutputs: true,
+         filesystem: true
+     })
     }
-      onClick() {
-          this.captureData = this.draw();
-        }
+    
+    onClick() {
+       this.captureData = this.draw();
+    }
+    
       startCamera() {
           window.navigator.mediaDevices.getUserMedia(this.medias)
               .then(stream => this.videoElm.nativeElement.srcObject = stream)
@@ -48,70 +42,49 @@ export class AppComponent  implements AfterViewInit, DoCheck {
       }
 
       draw() {
+        this.canvasElm
+        this.reslt = ""
+        const WIDTH = 100
+        const HEIGHT = 100;
+        const ctx = this.canvasElm.nativeElement.getContext('2d') as CanvasRenderingContext2D;
+        ctx.clearRect( 0, 0, WIDTH, HEIGHT);
+        this.canvasElm.nativeElement.width  = WIDTH;
+        this.canvasElm.nativeElement.height = HEIGHT;
 
-          const WIDTH = this.videoElm.nativeElement.clientWidth;
-          const HEIGHT = this.videoElm.nativeElement.clientHeight;
-          // canvasを用意する
-          const ctx = this.canvasElm.nativeElement.getContext('2d') as CanvasRenderingContext2D;
-          this.canvasElm.nativeElement.width  = WIDTH;
-          this.canvasElm.nativeElement.height = HEIGHT;
-
-          // canvasの描画をしつつBase64データを取る
-
-          const model = new KerasJS.Model({
-              filepath: '/assets/models/model1d.bin',
-              transferLayerOutputs: true,
-              filesystem: true
-          })
-          if (ctx) {
-                  console.log('inin')
-                  // this.context.drawImage(img, 0, 0,100,100);
-                  this.canvasElm.nativeElement.toDataURL(ctx.drawImage(this.videoElm.nativeElement, 0, 0, WIDTH, HEIGHT));
-                  setTimeout(()=>{
-                  const imageData  = ctx.getImageData(0,0,25,25);
-                  let input =  []
-                  let r = []
-                  let g = []
-                  let b = []
-                  for (var i = 0; i < imageData.data.length; i += 4){
-                      r.push(imageData.data[i])
-                      g.push(imageData.data[i+1])
-                      b.push(imageData.data[i+2])
-                  }
-                  input = r.concat(g);
-                  input = input.concat(b);
-
-                  console.log(input)
-                      model.ready()
-                          .then(() => {
-                              const inputData = {
-                                  input: new Float32Array(input)
-                              }
-                              return model.predict(inputData)
-                          })
-                              .then(outputData => {
-                                  console.log('yay!')
-                                  console.log(outputData)
-                                  if (outputData.output[0] > 0.8){
-                                     this.reslt = "オレンジ"
-                                  }else if (outputData.output[1] > 0.8){
-                                     this.reslt = "りんご"
-                                  }else{
-                                      this.reslt = 'わからない'
-                                  }
-                              })
-                                  .catch(err => {
-                                      console.error(err)
-                              })
-
-
-                  },100)
-               // console.log(imageData.data.transpose(2, 0, 1))
-              }
-              // let data = imageData.data.transpose(2, 0, 1)
-              //let input = new Float32Array(data.data) 
-         }
-      }
-
-
+        if (ctx) {
+            ctx.drawImage(this.videoElm.nativeElement, 0, 0, 100, 100))
+            setTimeout(()=>{
+                const imageData = ctx.getImageData(0,0,100,100);
+                let input =  []
+                let r = []
+                let g = []
+                let b = []
+                for (var i = 0; i < imageData.data.length; i += 4){
+                    r.push(imageData.data[i]/255)
+                    g.push(imageData.data[i+1]/255)
+                    b.push(imageData.data[i+2]/255)
+                }
+                input = r.concat(g);
+                input = input.concat(b);
+                this.model.ready()
+                .then(() => {
+                    const inputData = {
+                        input: new Float32Array(input)
+                    }
+                    return this.model.predict(inputData)
+                }).then(outputData => {
+                    console.log(outputData)
+                    if (outputData.output[0] === 1){
+                        this.reslt = "オレンジ"
+                    }else if(outputData.output[1] === 1 ){
+                        this.reslt = "りんご"
+                    }else{
+                        this.reslt = "わからない"
+                    }
+                }).catch(err => {
+                    console.error(err)
+                })
+            },100)
+        }
+    }
 }
